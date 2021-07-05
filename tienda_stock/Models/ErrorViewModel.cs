6ca423533664df521963aaa0d4e3c1e50ded23f4
Sephirok <luismanuelpd@gmail.com>
2021-07-05 16:54:19 -0400

    first commit

---
+++ tienda_stock/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tienda_stock.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
